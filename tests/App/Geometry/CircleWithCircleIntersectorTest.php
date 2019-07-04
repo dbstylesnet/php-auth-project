91984fcff7b4fb0afa9d73fdbc7752d606a78a1e
dbstylesnet <db@dbstyles.net>
2019-07-04 01:15:32 +0100

    AddedCircleWithCircleTests

---
+++ tests/App/Geometry/CircleWithCircleIntersectorTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace AppTest\Geometry;
+
+use App\Geometry\Rectangle;
+use App\Geometry\Point;
+use App\Geometry\Circle;
+use App\Geometry\RectangleWithCircleIntersector;
+use PHPUnit\Framework\TestCase;
+
+class CircleWithCircleIntersectorTest extends TestCase
+{
+
+    private $intersector;
+
+    protected function setUp()
+    {
+        $this->intersector = new CircleWithCircleIntersector();    
+    }
+
+    /**
+     * Shouldn't intersect
+     * Shouldn't contain
+     */
+    public function testIsIntersectIsContain() 
+    { 
+        $p1 = new Point(1, 1);
+        $a = new Circle(1, $p1);
+        $p2 = new Point(5, 1);
+        $b = new Circle(1, $p2);
+
+        $this->assertEquals(false, $this->intersector->isIntersect($a, $b));
+    }
+
+    /**
+     * Should intersect
+     * Shouldn't contain
+     * 1 intersect point
+     */
+    public function testIsIntersectIsContainCase2() 
+    { 
+        $p1 = new Point(1, 1);
+        $a = new Circle(1, $p1);
+        $p2 = new Point(3, 1);
+        $b = new Circle(1, $p2);
+
+        $this->assertEquals(true, $this->intersector->isIntersect($a, $b));
+    }
+
+    /**
+     * Should intersect
+     * Should contain
+     */
+    public function testIsIntersectIsContainCase3() 
+    { 
+        $p1 = new Point(1.5, 1.5);
+        $a = new Circle(1, $p1);
+        $p2 = new Point(1.5, 1.5);
+        $b = new Circle(3, $p2);
+
+        $this->assertEquals(true, $this->intersector->isIntersect($a, $b));
+    }
+
+    /**
+     * Should intersect
+     * Shouldn't contain
+     * 2 intersect points
+     */
+    public function testIsIntersectIsContainCase3() 
+    { 
+        $p1 = new Point(1, 1);
+        $a = new Circle(1, $p1);
+        $p2 = new Point(1.5, 1);
+        $b = new Circle(1, $p2);
+
+        $this->assertEquals(true, $this->intersector->isIntersect($a, $b));
+    }
+
+    public function testShouldThrowAnExceptionForUnsupportedTypes()
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->intersector->isIntersect(new Rectangle(new Point(1, 3), new Point(3, 1)), new Circle(1, new Point(1, 1)));
+    }
+}
