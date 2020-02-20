3a319a87deec49aafabd780e6d40d6806e5e5f96
Dariusz <db@dbstyles.net>
2020-02-20 12:07:59 +0000

    Reducers and actions structure

---
+++ frontend/src/actions/signIn.js
@@ -0,0 +1,7 @@
+const logIn = () => {
+    return {
+        type: 'SIGN_IN'
+    }
+}
+
+export { logIn }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
+import allReducer from './reducers'
+
+const store = createStore(allReducer)
 
 ReactDOM.render(
-    <Provider>
+    <Provider store={store}>
         <App />
     </Provider>, 
     document.getElementById('root')
